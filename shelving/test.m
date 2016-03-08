242ddf4c83e89b7d03aa1c1f758f82e4817af905
Ang Li <angleedomain@gmail.com>
2016-03-08 20:52:05 +1100

    update test.m

--- shelving/test.m
+++ shelving/test.m
@@ -1,3 +1,7 @@
 load('test_signal');
 [shelf_b, shelf_a] = shelving(6, 1000, 16E3, 0.9, 'Treble_Shelf');
+
+shelf_b = shelf_b/4;
+shelf_a = shelf_a/4;
+
 after_preemphasis = filter(shelf_b, shelf_a, x);
