749767c1022c50e51ba71edca17749f7a61af523
Ang Li <angleedomain@gmail.com>
2016-03-10 20:33:06 +1100

    rename

--- melf/melf_filter.m
+++ melf/melf_filter.m
@@ -11,20 +11,20 @@ border = melf_border();
 filter_gain = zeros(20, 256);
 energy_melband = zeros(1, 20);
 
-for filter_num = 2:21
-	x_length = border(filter_num+1) - border(filter_num-1);
-	x_length_inc = border(filter_num) - border(filter_num-1);
-	x_length_dec = border(filter_num+1) - border(filter_num);
+for bank_num = 1:20
+	x_length = border(bank_num+2) - border(bank_num);
+	x_length_inc = border(bank_num+1) - border(bank_num);
+	x_length_dec = border(bank_num+2) - border(bank_num+1);
 
 	for index = 0:x_length_inc-1
-		filter_gain(filter_num-1, index+1) = index / x_length_inc;
+		filter_gain(bank_num, index+1) = index / x_length_inc;
 	end
 
 	for index = 0:x_length_dec
-		filter_gain(filter_num-1, index+x_length_inc+1) = 1 - index / x_length_dec;
+		filter_gain(bank_num, index+x_length_inc+1) = 1 - index / x_length_dec;
 	end
 
 	for index = 1:x_length+1
-		energy_melband(filter_num-1) = energy_melband(filter_num-1) + power(border(filter_num-1)+index) * filter_gain(filter_num-1, index);
+		energy_melband(bank_num) = energy_melband(bank_num) + power(border(bank_num)+index) * filter_gain(bank_num, index);
 	end
 end
