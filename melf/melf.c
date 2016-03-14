8492d63e62faa1112fa72a97f2a98bde7ba6f570
Ang Li <angleedomain@gmail.com>
2016-03-14 16:01:38 +1100

    update melf.c

--- melf/melf.c
+++ melf/melf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-static float bank_gain[20][58] = {0.0};
+static float bank_gain[20][59] = {0.0};
 
 void calc_bank_gain(void)
 {
-	int bank_border[] = { 0, 3, 6, 10, 14, 18, 24, 29, 36, 44, 52, 61, 72, 84, 98, 113, 130, 150, 172, 197, 225, 256 };
+	int bank_border[] = { 4, 361, 764, 1218, 1730, 2307, 2958, 3692, 4519, 5452, 6503, 7689, 9025, 10532, 12231, 14146, 16305, 18740, 21484, 24578, 28067, 32000 };
 
 	float temp;
 
@@ -13,14 +13,23 @@ void calc_bank_gain(void)
 		int x_length_inc = bank_border[bank_num+1] - bank_border[bank_num];
 		int x_length_dec = bank_border[bank_num+2] - bank_border[bank_num+1];
 
-		int index;
-		for (index = 1; index < x_length_inc; index++) {
-			temp = (float)index / (float)x_length_inc;
+		int offset = bank_border[bank_num]/125;
+
+		int index = 1;
+		int frequency_x4 = (index+offset) * 125;
+
+		while (frequency_x4 <= bank_border[bank_num+1]) {
+			temp = (float)(frequency_x4 - bank_border[bank_num]) / (float)x_length_inc;
 			bank_gain[bank_num][index-1] = temp;
+			index++;
+			frequency_x4 = (index+offset) * 125;
 		}
-		for (index = 0; index < x_length_dec; index++) {
-			temp = 1 - (float)index / (float)x_length_dec;
-			bank_gain[bank_num][x_length_inc + index-1] = temp;
+
+		while( frequency_x4 <= bank_border[bank_num+2] ) {
+			temp = 1 - (float)(frequency_x4 - bank_border[bank_num+1]) / (float)x_length_dec;
+			bank_gain[bank_num][index-1] = temp;
+			index++;
+			frequency_x4 = (index+offset) * 125;
 		}
 	}
 }
@@ -31,7 +40,7 @@ int main()
 
 	int row, column;
 	for (row = 0; row < 20; row++) {
-		for (column = 0; column < 58; column++) {
+		for (column = 0; column < 59; column++) {
 			printf("%f\t", bank_gain[row][column]);
 		}
 		printf("\n");
