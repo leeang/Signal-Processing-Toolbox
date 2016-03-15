8627e5623178353265da0ee4f721c6bd6108f1e9
Ang Li <angleedomain@gmail.com>
2016-03-15 21:34:03 +1100

    new print matrix

---
+++ print_matrix.m
@@ -0,0 +1,25 @@
+clear;
+data = rand(3, 5);
+
+[M, N] = size(data);
+
+filename = 'matrix.c';
+fid = fopen(filename, 'w');
+
+fprintf(fid, 'float data[%d][%d] = {', M, N);
+
+prefix1 = '';
+for m=1:M
+	fprintf(fid, '%s\n', prefix1);
+	fprintf(fid, '\t{');
+	prefix2 = '';
+	for n=1:N
+		fprintf(fid, '%s', prefix2);
+		fprintf(fid, '%f', data(m, n));
+		prefix2 = ', ';
+	end
+	fprintf(fid, '}');
+	prefix1 = ',';
+end
+
+fprintf(fid, '\n};\n');
