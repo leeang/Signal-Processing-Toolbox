6be7623e52a6534dfdd6d4d6636a833591536507
Ang Li <angleedomain@gmail.com>
2016-03-17 20:16:48 +1100

    update threshold.m

--- shelving/test.m
+++ shelving/test.m
@@ -1,7 +1,17 @@
+clear;
+
 load('test_signal');
+WINDOW_LENGTH = 256;
+
 [shelf_b, shelf_a] = shelving(6, 1000, 16E3, 0.9, 'Treble_Shelf');
 
 shelf_b = shelf_b/4;
 shelf_a = shelf_a/4;
 
 after_preemphasis = filter(shelf_b, shelf_a, x);
+energy = sum(after_preemphasis .^ 2);
+
+after_hamming = hamming(WINDOW_LENGTH) .* after_preemphasis;
+energy = sum(after_hamming .^ 2);
+
+zc_num = zc_count(x);
