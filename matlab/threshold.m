a27cce91efbe4e58286e0e932e7775684aa17979
Ang Li <angleedomain@gmail.com>
2016-03-17 21:27:30 +1100

    update threshold

--- matlab/threshold.m
+++ matlab/threshold.m
@@ -1,7 +1,9 @@
 clear;
 
-load('test_signal');
-WINDOW_LENGTH = 256;
+load('data_512');
+WINDOW_LENGTH = 512;
+
+energy = sum(x .^ 2);
 
 [shelf_b, shelf_a] = shelving(6, 1000, 16E3, 0.9, 'Treble_Shelf');
 
@@ -9,9 +11,9 @@ shelf_b = shelf_b/4;
 shelf_a = shelf_a/4;
 
 after_preemphasis = filter(shelf_b, shelf_a, x);
-energy = sum(after_preemphasis .^ 2);
+energy_after_preemphasis = sum(after_preemphasis .^ 2);
 
 after_hamming = hamming(WINDOW_LENGTH) .* after_preemphasis;
-energy = sum(after_hamming .^ 2);
+energy_after_hamming = sum(after_hamming .^ 2);
 
 zc_num = zc_count(x);
