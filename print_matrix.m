40dc9899f309dc1c7a1c016b6ce4266e7cf8bfad
Ang Li <angleedomain@gmail.com>
2016-03-17 20:47:38 +1100

    update print_matrix

--- print_matrix.m
+++ print_matrix.m
@@ -1,25 +1,24 @@
-clear;
-data = rand(3, 5);
+function print_matrix(data)
+	[M, N] = size(data);
 
-[M, N] = size(data);
+	filename = 'matrix.c';
+	fid = fopen(filename, 'w');
 
-filename = 'matrix.c';
-fid = fopen(filename, 'w');
+	fprintf(fid, 'float data[%d][%d] = {', M, N);
 
-fprintf(fid, 'float data[%d][%d] = {', M, N);
-
-prefix1 = '';
-for m=1:M
-	fprintf(fid, '%s\n', prefix1);
-	fprintf(fid, '\t{');
-	prefix2 = '';
-	for n=1:N
-		fprintf(fid, '%s', prefix2);
-		fprintf(fid, '%f', data(m, n));
-		prefix2 = ', ';
+	prefix1 = '';
+	for m=1:M
+		fprintf(fid, '%s\n', prefix1);
+		fprintf(fid, '\t{');
+		prefix2 = '';
+		for n=1:N
+			fprintf(fid, '%s', prefix2);
+			fprintf(fid, '%f', data(m, n));
+			prefix2 = ', ';
+		end
+		fprintf(fid, '}');
+		prefix1 = ',';
 	end
-	fprintf(fid, '}');
-	prefix1 = ',';
-end
 
-fprintf(fid, '\n};\n');
+	fprintf(fid, '\n};\n');
+end
