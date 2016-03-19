775606ddc91c3b3ee09973e69149fa7939255097
Ang Lee <angleedomain@gmail.com>
2016-03-19 19:53:04 +1100

    update mfcc.m

--- matlab/mfcc.m
+++ matlab/mfcc.m
@@ -34,11 +34,25 @@ shelf_a = shelf_a/4;
 
 after_preemphasis = filter(shelf_b, shelf_a, x);
 
+frame_data = zeros(WINDOW_LENGTH, 1);
+voiced_cepstrum = zeros(180, 12);
+voiced_cepstrum_index = 1;
+
 frame_offset = 0;
 
 for frame_num = 1:FRAME_NUM
 	for index = 1:WINDOW_LENGTH
-		data = x(index+frame_offset);
+		frame_data(index) = after_preemphasis(index+frame_offset);
 	end
+
+	frame_data = hamming(WINDOW_LENGTH) .* frame_data;
+	energy = sum(frame_data .^ 2);
+	zc_num = zc_count(frame_data);
+
+	if energy > 0.1
+		voiced_cepstrum(voiced_cepstrum_index, :) = calc_mfcc(frame_data);
+		voiced_cepstrum_index = voiced_cepstrum_index + 1;
+	end
+
 	frame_offset = frame_offset + WINDOW_LENGTH/2;
 end
