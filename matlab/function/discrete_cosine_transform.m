e2765f6c21b93440a50b6652ff39b4be7953c8f8
Ang Lee <angleedomain@gmail.com>
2016-03-24 18:00:01 +1100

    update dct matlab function

--- matlab/function/discrete_cosine_transform.m
+++ matlab/function/discrete_cosine_transform.m
@@ -7,7 +7,7 @@ function cepstrum_coefficient = discrete_cosine_transform(energy_melband)
 	for n = 0:FEAT_NUM-1
 		sum = 0;
 		for k = 0:BANK_NUM/2-1
-			sum = sum + (energy_melband(k+1) + (-1)^n * energy_melband(20-k)) * dct_coef(n+1, k+1);
+			sum = sum + (energy_melband(k+1) + (-1)^n * energy_melband(BANK_NUM-k)) * dct_coef(n+1, k+1);
 		end
 		cepstrum_coefficient(n+1) = sqrt(2/BANK_NUM) * sum;
 	end
