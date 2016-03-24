1fed22543d9f6f99d220a63a32b73a4a74ef36bf
Ang Li <angleedomain@gmail.com>
2016-03-24 22:39:27 +1100

    update export emis

--- export_emis.m
+++ export_emis.m
@@ -15,19 +15,19 @@ for model_num = 1:WORD_NUM
 	data = Model{model_num};
 
 	mu_matrix = zeros(STATE_NUM, FEAT_NUM);
-	for index = 1:STATE_NUM
-		mu_matrix(index, :) = data.Mu{index};
+	for state = 1:STATE_NUM
+		mu_matrix(state, :) = data.Mu{state};
 	end
 	mu{model_num} = mu_matrix;
 
 	inv_Var_matrix = zeros(STATE_NUM, FEAT_NUM);
-	for index = 1:STATE_NUM
-		Var = data.Var{index} .* eye(FEAT_NUM);
-		inv_Var_matrix(index, :) = diag(inv(Var));
+	for state = 1:STATE_NUM
+		Var = data.Var{state} .* eye(FEAT_NUM);
+		inv_Var_matrix(state, :) = diag(inv(Var));
 
 		det_Var = det(Var);
 		det_Var = 1 / sqrt(det_Var * (2 * pi)^FEAT_NUM);
-		det_part(model_num, index) = det_Var;
+		det_part(model_num, state) = det_Var;
 	end
 	inv_Var{model_num} = inv_Var_matrix;
 
