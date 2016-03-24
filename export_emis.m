12363ffc7512b4659c264e4e970a1fa6fb2a70f9
Ang Li <angleedomain@gmail.com>
2016-03-24 22:46:29 +1100

    update export emis

--- export_emis.m
+++ export_emis.m
@@ -14,24 +14,19 @@ trans = cell(1, WORD_NUM);
 for model_num = 1:WORD_NUM
 	data = Model{model_num};
 
-	mu_matrix = zeros(STATE_NUM, FEAT_NUM);
 	for state = 1:STATE_NUM
-		mu_matrix(state, :) = data.Mu{state};
-	end
-	mu{model_num} = mu_matrix;
+		mu{model_num}(state, :) = data.Mu{state};
 
-	inv_Var_matrix = zeros(STATE_NUM, FEAT_NUM);
-	for state = 1:STATE_NUM
 		Var = data.Var{state} .* eye(FEAT_NUM);
-		inv_Var_matrix(state, :) = diag(inv(Var));
+
+		inv_Var{model_num}(state, :) = diag(inv(Var));
 
 		det_Var = det(Var);
 		det_Var = 1 / sqrt(det_Var * (2 * pi)^FEAT_NUM);
 		det_part(model_num, state) = det_Var;
 	end
-	inv_Var{model_num} = inv_Var_matrix;
 
-    trans{model_num} = data.Trans;
+	trans{model_num} = data.Trans;
 end
 
 print_3D(mu, 'mu', '%e');
