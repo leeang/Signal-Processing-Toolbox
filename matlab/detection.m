657fbd8be26d6579571dd950756363d9a34cf0f3
Ang Li <angleedomain@gmail.com>
2016-03-27 11:12:23 +1100

    new detection.m

---
+++ matlab/detection.m
@@ -0,0 +1,37 @@
+clear;
+
+ABSPATH = pwd;
+% identify current folder (absolute path)
+addpath([ABSPATH '/function']);
+% add function path
+
+global FRAME_NUM;
+FRAME_NUM = 180;
+
+global WINDOW_LENGTH;
+WINDOW_LENGTH = 512;
+
+global BANK_NUM;
+BANK_NUM = 20;
+
+global FEAT_NUM;
+FEAT_NUM = 12;
+
+global border;
+border = get_bank_border();
+
+global bank_gain;
+calc_bank_gain();
+
+global dct_coef;
+calc_dct_coef();
+
+global shelf_b;
+global shelf_a;
+
+[shelf_b, shelf_a] = shelving(6, 1000, 16E3, 0.9, 'Treble_Shelf');
+
+shelf_b = shelf_b/4;
+shelf_a = shelf_a/4;
+
+load('data');
