bddf1c2a71757ac97fa04dfad65d64cce6110a79
Ang Li <angleedomain@gmail.com>
2016-04-03 13:29:35 +1000

    lms_circular function.c

--- pc/lms_circular.c
+++ pc/lms_circular.c
@@ -2,66 +2,76 @@
 
 #include "lms_data.c"
 
-#define TOTAL_LENGTH	48000
+#define SAMPLE_LENGTH	48000
 #define LMS_LENGTH		300
 #define LMS_STEP_SIZE	0.04
 
-double err[TOTAL_LENGTH] = { 0.0 };
 double weights[LMS_LENGTH] = { 0.0 };
 double noise_buffer[LMS_LENGTH] = { 0.0 };
-double square_sum = 0;
+double lms_square_sum = 0;
 
-int main() {
+double err1[SAMPLE_LENGTH] = {0};
+double err2[SAMPLE_LENGTH] = {0};
+
+void lms_circular(double cmd_noise_in[], double noise_in[], double err_out[], int arr_length) {
 	double output;
 	double new_step_size;
 
-	int n;
-	int k;
+	int n, k;
 
 	for (n = 0; n < LMS_LENGTH; n++) {
 		output = 0;
 		for (k = 0; k <= n; k++) {
-			output += weights[k] * noise[n-k];
+			output += weights[k] * noise_in[n-k];
 		}
 		for (k = n+1; k < LMS_LENGTH; k++) {
 			output += weights[k] * noise_buffer[n-k+LMS_LENGTH];
 		}
 
-		err[n] = cmd_noise[n] - output;
+		err_out[n] = cmd_noise_in[n] - output;
 
-		square_sum = square_sum - noise_buffer[n] * noise_buffer[n] + noise[n] * noise[n];
-		new_step_size = LMS_STEP_SIZE / (1+square_sum);
+		lms_square_sum = lms_square_sum - noise_buffer[n] * noise_buffer[n] + noise_in[n] * noise_in[n];
+		new_step_size = LMS_STEP_SIZE / (1+lms_square_sum);
 
 		for (k = 0; k <= n; k++) {
-			weights[k] += new_step_size * err[n] * noise[n-k];
+			weights[k] += new_step_size * err_out[n] * noise_in[n-k];
 		}
 		for (k = n+1; k < LMS_LENGTH; k++) {
-			weights[k] += new_step_size * err[n] * noise_buffer[n-k+LMS_LENGTH];
+			weights[k] += new_step_size * err_out[n] * noise_buffer[n-k+LMS_LENGTH];
 		}
 	}
 
-	for (n = LMS_LENGTH; n < TOTAL_LENGTH; n++) {
+	for (n = LMS_LENGTH; n < arr_length; n++) {
 		output = 0;
 		for (k = 0; k < LMS_LENGTH; k++) {
-			output += weights[k] * noise[n-k];
+			output += weights[k] * noise_in[n-k];
 		}
 
-		err[n] = cmd_noise[n] - output;
+		err_out[n] = cmd_noise_in[n] - output;
 
-		square_sum = square_sum - noise[n-LMS_LENGTH] * noise[n-LMS_LENGTH] + noise[n] * noise[n];
-		new_step_size = LMS_STEP_SIZE / (1+square_sum);
+		lms_square_sum = lms_square_sum - noise_in[n-LMS_LENGTH] * noise_in[n-LMS_LENGTH] + noise_in[n] * noise_in[n];
+		new_step_size = LMS_STEP_SIZE / (1+lms_square_sum);
 
 		for (k = 0; k < LMS_LENGTH; k++) {
-			weights[k] += new_step_size * err[n] * noise[n-k];
+			weights[k] += new_step_size * err_out[n] * noise_in[n-k];
 		}
 	}
 
 	for (n = 0; n < LMS_LENGTH; n++) {
-		noise_buffer[n] = noise[TOTAL_LENGTH-LMS_LENGTH+n];
+		noise_buffer[n] = noise_in[arr_length-LMS_LENGTH+n];
 	}
+}
 
-	for (n = 0; n < TOTAL_LENGTH; n++) {
-		printf("%e\n", err[n]);
+int main() {
+	lms_circular(cmd_noise, noise, err1, SAMPLE_LENGTH);
+	lms_circular(cmd_noise, noise, err2, SAMPLE_LENGTH);
+
+	int n;
+	for (n = 0; n < SAMPLE_LENGTH; n++) {
+		printf("%e\n", err1[n]);
+	}
+	for (n = 0; n < SAMPLE_LENGTH; n++) {
+		printf("%e\n", err2[n]);
 	}
 
 	return 0;
