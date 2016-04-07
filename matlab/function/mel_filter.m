a3e5dd7928d5574056408127f1136db1fdd2c3c8
Ang Li <angleedomain@gmail.com>
2016-04-07 14:23:00 +1000

    update mel_filter.m

--- matlab/function/mel_filter.m
+++ matlab/function/mel_filter.m
@@ -1,23 +1,14 @@
 function energy_melband = mel_filter(power_spectrum)
 	global BANK_NUM;
-	global border;
 	global bank_gain;
+	global bank_gain_index_offset;
+	global fft_index_offset;
+	global fft_index_length;
 
 	energy_melband = zeros(1, BANK_NUM);
-	for bank_num = 1:BANK_NUM
-		offset = floor(border(bank_num)/125);
-		L = floor(border(bank_num+2)/125) - floor(border(bank_num)/125);
-
-		if bank_num == 1
-			filter_gain_offset = floor(border(1)/125);
-		elseif bank_num == 2
-			filter_gain_offset = floor(border(3)/125) - floor(border(1)/125);
-		else
-			filter_gain_offset = floor(border(bank_num+1)/125) + floor(border(bank_num)/125) - floor(border(2)/125) - floor(border(1)/125);
-		end
-
-		for index = 1:L
-			energy_melband(bank_num) = energy_melband(bank_num) + power_spectrum(index+offset+1) * bank_gain(index+filter_gain_offset);
+	for bank_index = 1:BANK_NUM
+		for index = 1:fft_index_length(bank_index)
+			energy_melband(bank_index) = energy_melband(bank_index) + power_spectrum(index + fft_index_offset(bank_index)) * bank_gain(index + bank_gain_index_offset(bank_index));
 		end
 	end
 
