c0c0e3d8cb23d2c7ce02808f29ade6600e60f7ce
Ang Li <angleedomain@gmail.com>
2016-05-02 02:11:51 +1000

    update header

--- dsp/header.h
+++ dsp/header.h
@@ -7,18 +7,18 @@
 
 #define PI	3.14159265358979323846
 
-#define WINDOW_LENGTH	512
+#define WINDOW_LENGTH		512
 #define WINDOW_LENGTH_HALF	256
-#define FRAME_NUM		180
-#define SAMPLE_LENGTH	46336
+#define FRAME_NUM			180
+#define SAMPLE_LENGTH		46336
 // (FRAME_NUM+1) * WINDOW_LENGTH/2
 
-#define ENERGY_THRSH	0.04
+#define ENERGY_THRSH		0.04
 
 /* FIR filter */
-#define BUFFER_SIZE		2
+#define BUFFER_SIZE			2
 	//define the buffer size
-#define INDEX(CURRENT)	( ((CURRENT) + BUFFER_SIZE) % BUFFER_SIZE )
+#define INDEX(CURRENT)		( ((CURRENT) + BUFFER_SIZE) % BUFFER_SIZE )
 	// if an index is negative, a specified position from the end of the array will be returned.
 	// e.g. given an array x[8], x[INDEX(-1)] and x[INDEX(7)] both refer to x[7].
 /* /FIR filter */
@@ -27,19 +27,19 @@
 #define IS_ZC(X, Y)		( (X) > 0 && (Y) < 0 || (X) < 0 && (Y) > 0 )
 
 /* FFT */
-#define TWIDDLE_STRIDE	1
-#define DYNAMIC_SCALING	2
+#define TWIDDLE_STRIDE		1
+#define DYNAMIC_SCALING		2
 
 /* Mel filter */
-#define BANK_NUM		20
-#define BANK_NUM_HALF	10
-#define FEAT_NUM		13
+#define BANK_NUM			20
+#define BANK_NUM_HALF		10
+#define FEAT_NUM			13
 #define BANK_GAIN_LENGTH	477
 
 /* detection */
-#define STATE_NUM		5
-#define WORD_NUM		27
-#define Inf				10000
+#define STATE_NUM			5
+#define WORD_NUM			27
+#define Inf					10000
 
 #define EMIS(OBS_INDEX, STATE_INDEX)	(*(ptr_to_emis + (OBS_INDEX) * STATE_NUM + STATE_INDEX))
 #define PHI(OBS_INDEX, STATE_INDEX)		(*(ptr_to_phi + (OBS_INDEX) * STATE_NUM + STATE_INDEX))
