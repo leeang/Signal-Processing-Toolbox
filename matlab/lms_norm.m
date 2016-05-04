b5c0bce58ed056d4eb248e61cdc8c8440e79e8d6
Ang Lee <angleedomain@gmail.com>
2016-05-04 19:10:20 +1000

    udpate lms matlab

--- matlab/lms_norm.m
+++ matlab/lms_norm.m
@@ -5,12 +5,10 @@ addpath([ABSPATH '/function']);
 
 load('lms_data.mat');
 
-cmd_noise = [cmd_noise; cmd_noise];
-noise = [noise; noise];
-
-LMS_LENGTH = 300;
-STEP_SIZE = 0.04;
-TOTAL_LENGTH = length(cmd_noise);
+LMS_LENGTH = 128;
+STEP_SIZE = 0.1;
+TOTAL_LENGTH = 46336;
+UPDATE_INTERVAL = 4;
 
 weights = zeros(1, LMS_LENGTH);
 
@@ -28,11 +26,13 @@ for n = 1:LMS_LENGTH
 
 	err(n) = cmd_noise(n) - output;
 
+	if mod(n, UPDATE_INTERVAL) == 1
 	norm_factor = norm_factor + noise(n)^2;
 	new_step_size = STEP_SIZE / (1+norm_factor);
 
 	for k = 1:n
 		weights(k) = weights(k) + new_step_size * err(n) * noise(n-k+1);
+	end
 	end
 end
 
@@ -44,11 +44,13 @@ for n = (LMS_LENGTH+1):TOTAL_LENGTH
 
 	err(n) = cmd_noise(n) - output;
 
+	if mod(n, UPDATE_INTERVAL) == 1
 	norm_factor = norm_factor - noise(n-LMS_LENGTH)^2 + noise(n)^2;
 	new_step_size = STEP_SIZE / (1+norm_factor);
 
 	for k = 1:LMS_LENGTH
 		weights(k) = weights(k) + new_step_size * err(n) * noise(n-k+1);
+	end
 	end
 end
 
