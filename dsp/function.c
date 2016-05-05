25a6e388a887c8c741ef01b3d278e76094ded273
Ang Lee <angleedomain@gmail.com>
2016-05-05 14:31:23 +1000

    use round multiplications

--- dsp/function.c
+++ dsp/function.c
@@ -69,11 +69,11 @@ void pre_emphasis(fract32 data[], int arr_length)
 
 	int index;
 	for (index = 0; index < arr_length; index++) {
-		fract32 temp_b0 = mult_fr1x32x32(shelving_coef.b0, data[index]);
-		fract32 temp_b1 = mult_fr1x32x32(shelving_coef.b1, x1);
-		fract32 temp_b2 = mult_fr1x32x32(shelving_coef.b2, x2);
-		fract32 temp_a1 = mult_fr1x32x32(shelving_coef.a1, y1);
-		fract32 temp_a2 = mult_fr1x32x32(shelving_coef.a2, y2);
+		fract32 temp_b0 = multr_fr1x32x32(shelving_coef.b0, data[index]);
+		fract32 temp_b1 = multr_fr1x32x32(shelving_coef.b1, x1);
+		fract32 temp_b2 = multr_fr1x32x32(shelving_coef.b2, x2);
+		fract32 temp_a1 = multr_fr1x32x32(shelving_coef.a1, y1);
+		fract32 temp_a2 = multr_fr1x32x32(shelving_coef.a2, y2);
 
 		fract32 temp_b = add_fr1x32(add_fr1x32(temp_b0, temp_b1), temp_b2);
 		fract32 temp_a = add_fr1x32(temp_a1, temp_a2);
@@ -103,7 +103,7 @@ void hamming(fract32 data[])
 {
 	int index;
 	for (index = 0; index < WINDOW_LENGTH; index++) {
-		data[index] = mult_fr1x32x32(data[index], hamming_coef[index]);
+		data[index] = multr_fr1x32x32(data[index], hamming_coef[index]);
 	}
 }
 
@@ -113,7 +113,7 @@ fract32 calc_energy(fract32 data[])
 
 	int index;
 	for (index = 0; index < WINDOW_LENGTH; index++) {
-		fract32 power = mult_fr1x32x32(data[index], data[index]);
+		fract32 power = multr_fr1x32x32(data[index], data[index]);
 
 		power = shr_fr1x32(power, ENERGY_RIGHT_SHIFT);
 		// right shift before accumulation in case of overflow
@@ -157,7 +157,7 @@ void mel_filter(fract32 power[], float energy[], int block_exponent)
 
 		int index;
 		for (index = 0; index < fft_index_length[bank_num]; index++) {
-			fract32 filtered_power = mult_fr1x32x32(
+			fract32 filtered_power = multr_fr1x32x32(
 				power[index + fft_index_offset[bank_num]],
 				bank_gain[index + bank_gain_index_offset[bank_num]]
 			);
@@ -213,7 +213,7 @@ void calc_mfcc(fract32 frame_data_point[], float mfcc_row[])
 	fract32 power_spectrum[WINDOW_LENGTH_HALF];
 	for (i = 0; i < WINDOW_LENGTH_HALF; ++i) {
 		fract32 absolute = cabs_fr32(fft_spectrum[i]);
-		power_spectrum[i] = mult_fr1x32x32(absolute, absolute);
+		power_spectrum[i] = multr_fr1x32x32(absolute, absolute);
 	}
 
 	float bank_energy[BANK_NUM] = {0.0};
