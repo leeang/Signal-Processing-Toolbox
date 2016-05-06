fd584fb8d0f4dd4e9fa11692d096e762dce15888
Ang Li <angleedomain@gmail.com>
2016-05-06 10:06:50 +1000

    update pre emphasis

--- matlab/plot/pre_emphasis.m
+++ matlab/plot/pre_emphasis.m
@@ -1,27 +1,5 @@
 clear;
 
-load('../data');
-
-x = transpose(x);
-N = length(x);
-X = fft(x);
-X = abs(X);
-
-X = X(1:N/2);
-frequency_vector = (0:N/2-1) / N * 16E3;
-
-fig = figure;
-fig.Units = 'inches';
-pos = fig.Position;
-fig.PaperSize = [pos(3) pos(4)];
-fig.PaperPositionMode = 'Auto';
-stem(frequency_vector, X, 'marker', 'none');
-grid on;
-title('word \textit{zero} frequency spectrum', 'interpreter', 'latex');
-xlabel('frequency (Hz)', 'interpreter', 'latex');
-ylabel('magnitude (linear scale)', 'interpreter', 'latex');
-print('zero_fft', '-dpng', '-r300');
-
 a_shelf = [1 -1.52379693166806 0.649345913666990];
 b_shelf = [1.86185636138765 -3.10285167865561 1.36654429926689];
 [h_shelf, w_shelf] = freqz(b_shelf, a_shelf);
@@ -43,3 +21,25 @@ xlabel('frequency (Hz)', 'interpreter', 'latex');
 ylabel('magnitude (dB)', 'interpreter', 'latex');
 legend('shelving', 'high-pass FIR', 'location', 'southeast');
 print('pre_emphasis_filter', '-dpdf');
+
+load('../data');
+
+x = transpose(x);
+N = length(x);
+X = fft(x);
+X = abs(X);
+
+X = X(1:N/2);
+frequency_vector = (0:N/2-1) / N * 16E3;
+
+fig = figure;
+fig.Units = 'inches';
+pos = fig.Position;
+fig.PaperSize = [pos(3) pos(4)];
+fig.PaperPositionMode = 'Auto';
+stem(frequency_vector, X, 'marker', 'none');
+grid on;
+title('word \textit{zero} frequency spectrum', 'interpreter', 'latex');
+xlabel('frequency (Hz)', 'interpreter', 'latex');
+ylabel('magnitude (linear scale)', 'interpreter', 'latex');
+print('zero_fft', '-dpng', '-r300');
