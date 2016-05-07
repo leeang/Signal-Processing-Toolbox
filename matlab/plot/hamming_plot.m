625c7962c4fd0d26e47d1761cdab89e73e6105cf
Ang Li <angleedomain@gmail.com>
2016-05-07 12:35:10 +1000

    update hamming plot

--- matlab/plot/hamming_plot.m
+++ matlab/plot/hamming_plot.m
@@ -13,6 +13,43 @@ fig.PaperSize = [pos(3) pos(4)];
 fig.PaperPositionMode = 'Auto';
 
 subplot(2, 1, 1);
+plot(1:512, hamming(512));
+hold on;
+plot(513:1024, hamming(512));
+grid on;
+xlim([0 1024]);
+ax = gca;
+ax.XTick = linspace(0, 1024, 9);
+title('\texttt{hamming(512) without overlap}', 'interpreter', 'latex');
+xlabel('data point index', 'interpreter', 'latex');
+ylabel('gain', 'interpreter', 'latex');
+legend('window 1', 'window 2');
+
+subplot(2, 1, 2);
+plot(1:512, hamming(512));
+hold on;
+plot((1:512)+256, hamming(512));
+plot((1:512)+512, hamming(512));
+grid on;
+xlim([0 1024]);
+ax = gca;
+ax.XTick = linspace(0, 1024, 9);
+title('\texttt{hamming(512) with overlap 256}', 'interpreter', 'latex');
+xlabel('data point index', 'interpreter', 'latex');
+ylabel('gain', 'interpreter', 'latex');
+legend('window 1', 'window 2', 'window 3');
+
+annotation('rectangle', [0.46 0.12 0.12 0.33], 'LineStyle' ,'-.', 'color', 'cyan');
+
+print('hamming_overlap', '-dpdf');
+
+fig = figure;
+fig.Units = 'inches';
+pos = fig.Position;
+fig.PaperSize = [pos(3) pos(4)];
+fig.PaperPositionMode = 'Auto';
+
+subplot(2, 1, 1);
 plot(x);
 grid on;
 xlim([0 1024]);
@@ -34,3 +71,5 @@ title('after hamming window', 'interpreter', 'latex');
 xlabel('data point index', 'interpreter', 'latex');
 ylabel('amplitude', 'interpreter', 'latex');
 annotation('ellipse', [0.46 0.22 0.12 0.12], 'LineStyle' ,'-.', 'color', 'red');
+
+print('hamming_bell_shape', '-dpdf');
