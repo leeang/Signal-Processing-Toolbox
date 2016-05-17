1c8d777d22498834fa5777215ebf90e30131cbc3
Ang Lee <angleedomain@gmail.com>
2016-05-17 18:29:53 +1000

    update fft power

--- dsp/function.c
+++ dsp/function.c
@@ -208,10 +208,14 @@ void calc_mfcc(fract32 frame_data_point[], float mfcc_row[])
 
 	fract32 power_spectrum[WINDOW_LENGTH_HALF];
 	for (i = 0; i < WINDOW_LENGTH_HALF; ++i) {
+		fract32 absolute = cabs_fr32(fft_spectrum[i]);
+		power_spectrum[i] = multr_fr1x32x32(absolute, absolute);
+/*
 		power_spectrum[i] = add_fr1x32(
 			multr_fr1x32x32(fft_spectrum[i].re, fft_spectrum[i].re),
 			multr_fr1x32x32(fft_spectrum[i].im, fft_spectrum[i].im)
 		);
+*/
 	}
 
 	float bank_energy[BANK_NUM] = {0.0};
